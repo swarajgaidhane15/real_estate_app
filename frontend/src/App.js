2f7ea1710af8df7b965b13562a21f7876526666f
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2021-04-26 18:44:26 +0530

    completed listings and listingDetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Signin from "./containers/Signin";
 import Signup from "./containers/Signup";
 
 import NotFound from "./components/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
 
 import store from "./store";
 
@@ -29,7 +30,11 @@ function App() {
             <Route exact path="/about" component={About} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/listings" component={Listings} />
-            <Route exact path="/listingdetail/:id" component={ListingDetail} />
+            <PrivateRoute
+              exact
+              path="/listings/:id"
+              component={ListingDetail}
+            />
             <Route exact path="/login" component={Signin} />
             <Route exact path="/signup" component={Signup} />
             <Route component={NotFound} />
